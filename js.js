ef1fd7c08ba045b9bb0a7b3402a66ea139e75350
songo04 <100772355+songo04@users.noreply.github.com>
2022-03-24 20:40:54 +0100

    yhy

---
+++ js.js
@@ -0,0 +1,14 @@
+/*
+let ok = prompt("Jak masz na imie?");
+console.log("czesc"+" "+ok);
+function showMessage() 
+{
+    let userNumber = document.getElementById("userNumber").value;
+    let massage =("czesc"+" "+ok)
+    document.getElementById("massage").
+
+}
+*/
+function showMessage() {
+    document.getElementById("message").innerHTML = "Cześć"
+}
